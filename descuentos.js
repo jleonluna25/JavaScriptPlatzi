ce2246b62b7121114adb07e1c3d6a5473f887299
Jesus Leon <jleonluna25@gmail.com>
2021-09-28 18:00:51 -0500

    #6 Actualización de archivo js con correccion de errores

--- descuentos.js
+++ descuentos.js
@@ -5,12 +5,10 @@ function habilitarDescuento() {
     if (radio[0].checked) {
         document.getElementById("categoriasDescuento").removeAttribute("disabled");
         document.getElementById("cuponDescuento").disabled=true;
-        flag= 1;
     } else {
         if (radio[1].checked) {
             document.getElementById("cuponDescuento").removeAttribute("disabled");
             document.getElementById("categoriasDescuento").disabled=true;
-            flag= 2;
         } else {
             window.alert("No está funcionando bien la captura del valor del radio :/" + radio.value);
         }
@@ -19,19 +17,18 @@ function habilitarDescuento() {
 
 function calcularPrecio() {
     const precioBruto = document.getElementById("precioBruto");
-    window.alert(document.getElementById("categoriasDescuento").value);
-    document.getElementById("precioFinal").value=9;
-    if (flag == 1){/*reemplazar las condiciones por las mismas de la primera función*/ 
+    const radio1 = document.getElementsByName("tipoDescuento");
+    if (radio1[0].checked){
         const descuento = precioBruto.value*document.getElementById("categoriasDescuento").value/100;
         document.getElementById("precioFinal").value=precioBruto.value - descuento;
         document.getElementById("ahorro").value=descuento;
     }else{
-        if (flag == 2){
+        if (radio1[1].checked){
             const descuento = precioBruto.value*document.getElementById("cuponDescuento").value/100;
             document.getElementById("precioFinal").value=precioBruto.value - descuento;
             document.getElementById("ahorro").value=descuento;
         }else{
-            window.alert("Error con variable flag"+flag);
+            window.alert("Error con variable flag");
         }
 
     }
